e528cc925ab1a8358801f9d0aa218212fa2cf7b1
Indyuce <cym.peyrat@yahoo.fr>
2019-09-01 18:27:01 +0200

    update comments and player-stats.yml

--- src/main/java/net/Indyuce/mmocore/listener/option/VanillaExperienceOverride.java
+++ src/main/java/net/Indyuce/mmocore/listener/option/VanillaExperienceOverride.java
@@ -11,11 +11,20 @@ import net.Indyuce.mmocore.api.player.PlayerData;
 
 public class VanillaExperienceOverride implements Listener {
 
+	/*
+	 * when picking up exp orbs or any action like that
+	 */
 	@EventHandler
 	public void a(PlayerExpChangeEvent event) {
 		event.setAmount(0);
 	}
 
+	/*
+	 * this event is not supported by the expChangeEvent. since the event is
+	 * actually called before applying the enchant and consuming levels, we must
+	 * update the player level using a delayed task. setExpLevelCost(level) DOES
+	 * NOT WORK
+	 */
 	@EventHandler
 	public void b(EnchantItemEvent event) {
 		Bukkit.getScheduler().runTask(MMOCore.plugin, () -> event.getEnchanter().setLevel(PlayerData.get(event.getEnchanter()).getLevel()));
