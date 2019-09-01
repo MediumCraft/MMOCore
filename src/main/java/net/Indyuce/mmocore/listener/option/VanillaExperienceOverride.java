ecf0515149929845dd7851001986246a61f45cbe
Indyuce <cym.peyrat@yahoo.fr>
2019-09-01 18:08:34 +0200

    update level when enchanting

--- src/main/java/net/Indyuce/mmocore/listener/option/VanillaExperienceOverride.java
+++ src/main/java/net/Indyuce/mmocore/listener/option/VanillaExperienceOverride.java
@@ -1,16 +1,23 @@
 package net.Indyuce.mmocore.listener.option;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
 import net.Indyuce.mmocore.MMOCore;
+import net.Indyuce.mmocore.api.player.PlayerData;
 
 public class VanillaExperienceOverride implements Listener {
-	
+
 	@EventHandler
 	public void a(PlayerExpChangeEvent event) {
-		if (MMOCore.plugin.configManager.overrideVanillaExp)
-			event.setAmount(0);
+		event.setAmount(0);
+	}
+
+	@EventHandler
+	public void b(EnchantItemEvent event) {
+		Bukkit.getScheduler().runTask(MMOCore.plugin, () -> event.getEnchanter().setLevel(PlayerData.get(event.getEnchanter()).getLevel()));
 	}
 }
