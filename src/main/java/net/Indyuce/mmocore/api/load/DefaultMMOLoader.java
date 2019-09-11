0edbbd977a73ace6d3606cbaefbf051c313f13cb
Aria <ariahyrule@gmail.com>
2019-09-12 00:54:52 +0200

    Added 'harvestcrop' exp source

--- src/main/java/net/Indyuce/mmocore/api/load/DefaultMMOLoader.java
+++ src/main/java/net/Indyuce/mmocore/api/load/DefaultMMOLoader.java
@@ -13,6 +13,7 @@ import net.Indyuce.mmocore.api.experience.Profession;
 import net.Indyuce.mmocore.api.experience.source.BrewPotionExperienceSource;
 import net.Indyuce.mmocore.api.experience.source.EnchantItemExperienceSource;
 import net.Indyuce.mmocore.api.experience.source.FishItemExperienceSource;
+import net.Indyuce.mmocore.api.experience.source.HarvestCropExperienceSource;
 import net.Indyuce.mmocore.api.experience.source.KillMobExperienceSource;
 import net.Indyuce.mmocore.api.experience.source.MineBlockExperienceSource;
 import net.Indyuce.mmocore.api.experience.source.RepairItemExperienceSource;
@@ -113,6 +114,9 @@ public class DefaultMMOLoader implements MMOLoader {
 		if (config.getKey().equals("mineblock"))
 			return new MineBlockExperienceSource(profession, config);
 
+		if (config.getKey().equals("harvestcrop"))
+			return new HarvestCropExperienceSource(profession, config);
+		
 		if (config.getKey().equals("brewpotion"))
 			return new BrewPotionExperienceSource(profession, config);
 
