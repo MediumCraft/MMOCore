2df5d86f2545ea367a743034bd20d4a22d03479e
Indyuce <cym.peyrat@yahoo.fr>
2020-02-12 18:36:38 +0100

    !Field made final.. that's all

--- src/main/java/net/Indyuce/mmocore/api/quest/trigger/MessageTrigger.java
+++ src/main/java/net/Indyuce/mmocore/api/quest/trigger/MessageTrigger.java
@@ -1,6 +1,5 @@
 package net.Indyuce.mmocore.api.quest.trigger;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import net.Indyuce.mmocore.MMOCore;
@@ -8,7 +7,7 @@ import net.Indyuce.mmocore.api.load.MMOLineConfig;
 import net.Indyuce.mmocore.api.player.PlayerData;
 
 public class MessageTrigger extends Trigger {
-	private String message;
+	private final String message;
 
 	public MessageTrigger(MMOLineConfig config) {
 		super(config);
@@ -23,7 +22,6 @@ public class MessageTrigger extends Trigger {
 	}
 
 	private String format(Player player) {
-		message = ChatColor.translateAlternateColorCodes('&', message);
 		return MMOCore.plugin.placeholderParser.parse(player, message.replace("%player%", player.getName()));
 	}
 }
