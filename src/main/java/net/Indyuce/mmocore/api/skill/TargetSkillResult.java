8417ca60cab36a6f89a728eac7345a1f7a1d1ff1
Indyuce <cym.peyrat@yahoo.fr>
2020-03-19 13:38:57 +0100

    Fixed a 1.12 compatibility issue with ray traces

--- src/main/java/net/Indyuce/mmocore/api/skill/TargetSkillResult.java
+++ src/main/java/net/Indyuce/mmocore/api/skill/TargetSkillResult.java
@@ -1,11 +1,12 @@
 package net.Indyuce.mmocore.api.skill;
 
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.util.RayTraceResult;
 
 import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.api.skill.Skill.SkillInfo;
 import net.Indyuce.mmocore.api.util.MMOCoreUtils;
+import net.mmogroup.mmolib.MMOLib;
+import net.mmogroup.mmolib.api.MMORayTraceResult;
 
 public class TargetSkillResult extends SkillResult {
 	private LivingEntity target;
@@ -14,11 +15,11 @@ public class TargetSkillResult extends SkillResult {
 		super(data, skill);
 
 		if (isSuccessful()) {
-			RayTraceResult result = data.getPlayer().getWorld().rayTraceEntities(data.getPlayer().getEyeLocation(), data.getPlayer().getEyeLocation().getDirection(), range, entity -> MMOCoreUtils.canTarget(data.getPlayer(), entity));
-			if (result == null)
+			MMORayTraceResult result = MMOLib.plugin.getVersion().getWrapper().rayTrace(data.getPlayer(), range, entity -> MMOCoreUtils.canTarget(data.getPlayer(), entity));
+			if (!result.hasHit())
 				abort(CancelReason.OTHER);
 			else
-				target = (LivingEntity) result.getHitEntity();
+				target = (LivingEntity) result.getHit();
 		}
 	}
 
