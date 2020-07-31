dee0536e7437ca94e4afbd01a24aa7907dfd0e61
Ethan <ethan@plexiate.com>
2020-07-31 18:27:19 -0400

    Added new level up multiple event triggers. Example: If level = 2 it will trigger every 2 levels. 2,4,6,8... level-up-multiple-{level} level-up-multiple-{profession}-{level}

---
+++ src/main/java/net/Indyuce/mmocore/api/player/profess/event/trigger/MultipleLevelUpEventTrigger.java
@@ -0,0 +1,43 @@
+package net.Indyuce.mmocore.api.player.profess.event.trigger;
+
+import net.Indyuce.mmocore.api.event.PlayerLevelUpEvent;
+import net.Indyuce.mmocore.api.player.PlayerData;
+import net.Indyuce.mmocore.api.player.profess.PlayerClass;
+import net.Indyuce.mmocore.api.player.profess.event.EventTriggerHandler;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import java.text.DecimalFormat;
+
+public class MultipleLevelUpEventTrigger implements EventTriggerHandler {
+
+    @Override
+    public boolean handles(String event) {
+        return event.startsWith("level-up-multiple");
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void a(PlayerLevelUpEvent event) {
+        PlayerData player = event.getData();
+        PlayerClass profess = player.getProfess();
+        for (String t : profess.getEventTriggers()){
+            if (t.startsWith("level-up-multiple")) {
+                String[] split = t.split("-");
+                double multiple = Double.parseDouble(split[split.length-1]);
+                if (event.getNewLevel()/multiple % 1 == 0) {
+                    DecimalFormat f = new DecimalFormat("#");
+                    if (event.hasProfession()) {
+                        processTrigger(player, profess, "level-up-multiple-" + event.getProfession().getId().toLowerCase() + "-" + f.format(multiple));
+                    } else {
+                        processTrigger(player, profess, "level-up-multiple-" + f.format(multiple));
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void processTrigger(PlayerData player, PlayerClass profess, String trigger) {
+        if(profess.hasEventTriggers(trigger)) profess.getEventTriggers(trigger).getTriggers().forEach(t -> t.apply(player));
+    }
+}
