a34127fab8160395d62417489ede5d04d5541537
Joshua <Joshua@users.noreply.github.com>
2021-02-19 13:54:50 -0600

    josh is special.

--- src/main/java/net/Indyuce/mmocore/comp/mythicmobs/skill/handlers/PlayerLoginSkillHandler.java
+++ src/main/java/net/Indyuce/mmocore/comp/mythicmobs/skill/handlers/PlayerLoginSkillHandler.java
@@ -4,7 +4,6 @@ import net.Indyuce.mmocore.api.event.PlayerDataLoadEvent;
 import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.comp.mythicmobs.skill.MythicMobSkill;
 import net.Indyuce.mmocore.comp.mythicmobs.skill.PassiveMythicMobSkillHandler;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
 public class PlayerLoginSkillHandler extends PassiveMythicMobSkillHandler {
@@ -19,6 +18,6 @@ public class PlayerLoginSkillHandler extends PassiveMythicMobSkillHandler {
 
     @EventHandler
     private void event(PlayerDataLoadEvent e){
-            castSkill(PlayerData.get((Player) e));
+            castSkill(PlayerData.get( e.getPlayer()));
     }
 }
