ba41294748b30e2e92c00d9e5a532dbe64dae95f
Joshua <Joshua@users.noreply.github.com>
2021-02-19 15:16:49 -0600

    More fixes

--- src/main/java/net/Indyuce/mmocore/comp/mythicmobs/skill/handlers/PlayerLoginSkillHandler.java
+++ src/main/java/net/Indyuce/mmocore/comp/mythicmobs/skill/handlers/PlayerLoginSkillHandler.java
@@ -1,10 +1,10 @@
 package net.Indyuce.mmocore.comp.mythicmobs.skill.handlers;
 
-import net.Indyuce.mmocore.api.event.PlayerDataLoadEvent;
 import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.comp.mythicmobs.skill.MythicMobSkill;
 import net.Indyuce.mmocore.comp.mythicmobs.skill.PassiveMythicMobSkillHandler;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PlayerLoginSkillHandler extends PassiveMythicMobSkillHandler {
     /**
@@ -17,7 +17,7 @@ public class PlayerLoginSkillHandler extends PassiveMythicMobSkillHandler {
     }
 
     @EventHandler
-    private void event(PlayerDataLoadEvent e){
+    private void event(PlayerLoginEvent e){
             castSkill(PlayerData.get( e.getPlayer()));
     }
 }
