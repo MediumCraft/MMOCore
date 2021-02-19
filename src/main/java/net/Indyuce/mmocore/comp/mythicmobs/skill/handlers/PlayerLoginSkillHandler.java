8aa09295f53cb16b2234fbff616e330ba72ea3d0
Joshua <Joshua@users.noreply.github.com>
2021-02-19 13:27:12 -0600

    Changed how player_login works, might not ever be null now?

--- src/main/java/net/Indyuce/mmocore/comp/mythicmobs/skill/handlers/PlayerLoginSkillHandler.java
+++ src/main/java/net/Indyuce/mmocore/comp/mythicmobs/skill/handlers/PlayerLoginSkillHandler.java
@@ -1,15 +1,15 @@
 package net.Indyuce.mmocore.comp.mythicmobs.skill.handlers;
 
+import net.Indyuce.mmocore.api.event.PlayerDataLoadEvent;
 import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.comp.mythicmobs.skill.MythicMobSkill;
 import net.Indyuce.mmocore.comp.mythicmobs.skill.PassiveMythicMobSkillHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PlayerLoginSkillHandler extends PassiveMythicMobSkillHandler {
     /**
-     * Core class for all passive types
+     * Used when a player logins
      *
      * @param skill
      */
@@ -18,7 +18,7 @@ public class PlayerLoginSkillHandler extends PassiveMythicMobSkillHandler {
     }
 
     @EventHandler
-    private void event(PlayerLoginEvent e){
+    private void event(PlayerDataLoadEvent e){
             castSkill(PlayerData.get((Player) e));
     }
 }
