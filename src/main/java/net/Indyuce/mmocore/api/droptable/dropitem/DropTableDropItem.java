c1a98bc5a51c74d4a99a639177dcb2dfe57829f9
Indyuce <cym.peyrat@yahoo.fr>
2021-08-20 17:11:12 +0200

    fixed #252 double drops

--- src/main/java/net/Indyuce/mmocore/api/droptable/dropitem/DropTableDropItem.java
+++ src/main/java/net/Indyuce/mmocore/api/droptable/dropitem/DropTableDropItem.java
@@ -1,33 +1,33 @@
 package net.Indyuce.mmocore.api.droptable.dropitem;
 
-import org.apache.commons.lang.Validate;
-
+import io.lumine.mythic.lib.api.MMOLineConfig;
 import net.Indyuce.mmocore.MMOCore;
 import net.Indyuce.mmocore.api.droptable.DropTable;
 import net.Indyuce.mmocore.api.droptable.condition.ConditionInstance;
 import net.Indyuce.mmocore.api.loot.LootBuilder;
 import net.Indyuce.mmocore.api.player.PlayerData;
-import io.lumine.mythic.lib.api.MMOLineConfig;
+import org.apache.commons.lang.Validate;
 
 public class DropTableDropItem extends DropItem {
-	private final DropTable dropTable;
+    private final DropTable dropTable;
 
-	public DropTableDropItem(MMOLineConfig config) {
-		super(config);
+    public DropTableDropItem(MMOLineConfig config) {
+        super(config);
 
-		config.validate("id");
-		String id = config.getString("id");
+        config.validate("id");
+        String id = config.getString("id");
 
-		Validate.isTrue(MMOCore.plugin.dropTableManager.has(id), "Could not find drop table " + id);
-		this.dropTable = MMOCore.plugin.dropTableManager.get(id);
-	}
+        Validate.isTrue(MMOCore.plugin.dropTableManager.has(id), "Could not find drop table " + id);
+        this.dropTable = MMOCore.plugin.dropTableManager.get(id);
+    }
 
-	@Override
-	public void collect(LootBuilder builder) {
-		PlayerData data = builder.getEntity();
-		if(!data.isOnline()) return;
-		if (dropTable.areConditionsMet(new ConditionInstance(data.getPlayer())))
-			for (int j = 0; j < rollAmount(); j++)
-				builder.addLoot(dropTable.collect(builder));
-	}
+    @Override
+    public void collect(LootBuilder builder) {
+        PlayerData data = builder.getEntity();
+        if (!data.isOnline()) return;
+
+        if (dropTable.areConditionsMet(new ConditionInstance(data.getPlayer())))
+            for (int j = 0; j < rollAmount(); j++)
+                dropTable.collect(builder);
+    }
 }
