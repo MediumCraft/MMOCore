15cb4d7aa2fc59a5a57d22c4638245cca0115b25
Joshua <Joshua@users.noreply.github.com>
2021-09-03 20:27:20 -0500

    remove right click profile interact, was rushed.

--- src/main/java/net/Indyuce/mmocore/listener/option/PlayerProfileCheck.java
+++ src/main/java/net/Indyuce/mmocore/listener/option/PlayerProfileCheck.java
@@ -1,19 +1,14 @@
 package net.Indyuce.mmocore.listener.option;
 
-import io.lumine.mythic.lib.MythicLib;
-import io.lumine.mythic.lib.comp.target.InteractionType;
-import net.Indyuce.mmocore.api.player.PlayerData;
-import net.Indyuce.mmocore.manager.InventoryManager;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.inventory.Inventory;
 
 public class PlayerProfileCheck implements Listener {
 
     @EventHandler
     public void a(PlayerInteractEntityEvent event) {
+        /**
         if (event.getRightClicked().getType() != EntityType.PLAYER || !MythicLib.plugin.getEntities().canTarget(event.getPlayer(), event.getRightClicked(), InteractionType.SUPPORT_ACTION))
             return;
 
@@ -24,7 +19,10 @@ public class PlayerProfileCheck implements Listener {
          * Opening inventories like that to other players does NOT
          * necessarily works for any other custom inventory.
          * */
+        /**
         Inventory inv = InventoryManager.PLAYER_STATS.newInventory(PlayerData.get(event.getRightClicked().getUniqueId())).getInventory();
         event.getPlayer().openInventory(inv);
+        **/
     }
+
 }
