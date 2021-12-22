71cda8bf54cb52552130cf9af5878a4c5732d92f
Indyuce <cym.peyrat@yahoo.fr>
2021-12-22 13:59:23 +0100

    added enum comments

--- src/main/java/net/Indyuce/mmocore/api/player/profess/ClassOption.java
+++ src/main/java/net/Indyuce/mmocore/api/player/profess/ClassOption.java
@@ -2,22 +2,34 @@ package net.Indyuce.mmocore.api.player.profess;
 
 public enum ClassOption {
 
-	/*
-	 * is class by default
+	/**
+	 * If the class should be applied to newcomers
 	 */
 	DEFAULT,
 
-	/*
-	 * displays in the /class GUI
+	/**
+	 * If the class should in the /class GUI
 	 */
 	DISPLAY(true),
 
-	/*
-	 * only regen resource when out of combat
+	/**
+	 * Health only regens when out of combat
 	 */
 	OFF_COMBAT_HEALTH_REGEN,
+
+	/**
+	 * Mana only regens when out of combat
+	 */
 	OFF_COMBAT_MANA_REGEN,
+
+	/**
+	 * Stamina only regens when out of combat
+	 */
 	OFF_COMBAT_STAMINA_REGEN,
+
+	/**
+	 * Stellium only regens when out of combat
+	 */
 	OFF_COMBAT_STELLIUM_REGEN;
 
 	private final boolean def;
