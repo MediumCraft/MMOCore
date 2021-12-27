ef80ac265d4d134211fb48c1a7464867d6d0f7fa
Indyuce <cym.peyrat@yahoo.fr>
2021-12-27 14:26:23 +0100

    !no build

--- src/main/java/net/Indyuce/mmocore/command/rpg/admin/HideActionBarCommandTreeNode.java
+++ src/main/java/net/Indyuce/mmocore/command/rpg/admin/HideActionBarCommandTreeNode.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+
 import net.Indyuce.mmocore.api.player.PlayerData;
 import io.lumine.mythic.lib.commands.mmolib.api.CommandTreeNode;
 import io.lumine.mythic.lib.commands.mmolib.api.Parameter;
