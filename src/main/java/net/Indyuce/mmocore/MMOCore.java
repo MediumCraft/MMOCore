bc0b3f747f143ec9bda21032fb083184fa3b2dae
Indyuce <cym.peyrat@yahoo.fr>
2022-01-09 14:33:02 +0100

    /mmocore reload now reloads skills

--- src/main/java/net/Indyuce/mmocore/MMOCore.java
+++ src/main/java/net/Indyuce/mmocore/MMOCore.java
@@ -350,6 +350,7 @@ public class MMOCore extends LuminePlugin {
 
 		configManager = new ConfigManager();
 
+		MythicLib.plugin.getSkills().initialize(clearBefore);
 		skillManager.initialize(clearBefore);
 		mineManager.initialize(clearBefore);
 		partyManager.initialize(clearBefore);
