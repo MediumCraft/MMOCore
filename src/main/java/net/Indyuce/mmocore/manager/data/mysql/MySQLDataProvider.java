8a5a1b10703fe4bfa2cc16ebbe4ffca88cd8526b
Jules <peyrat.jules@yahoo.fr>
2022-03-01 14:03:56 +0100

    Fixed default database

--- src/main/java/net/Indyuce/mmocore/manager/data/mysql/MySQLDataProvider.java
+++ src/main/java/net/Indyuce/mmocore/manager/data/mysql/MySQLDataProvider.java
@@ -10,7 +10,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class MySQLDataProvider extends MMODataSource implements DataProvider {
 	private final MySQLPlayerDataManager playerManager = new MySQLPlayerDataManager(this);
 	private final YAMLGuildDataManager guildManager = new YAMLGuildDataManager();
-	
+
 	public MySQLDataProvider(FileConfiguration config) {
 		this.setup(config);
 	}
@@ -22,8 +22,9 @@ public class MySQLDataProvider extends MMODataSource implements DataProvider {
 			+ "INT(11) DEFAULT 0,skill_points INT(11) DEFAULT 0,attribute_points INT(11) "
 			+ "DEFAULT 0,attribute_realloc_points INT(11) DEFAULT 0,level INT(11) DEFAULT 1,"
 			+ "experience INT(11) DEFAULT 0,class VARCHAR(20),guild VARCHAR(20),last_login LONG,"
-			+ "attributes LONGTEXT,professions LONGTEXT,quests LONGTEXT,waypoints LONGTEXT,"
-			+ "friends LONGTEXT,skills LONGTEXT,bound_skills LONGTEXT,class_info LONGTEXT,PRIMARY KEY (uuid));");
+			+ "attributes LONGTEXT,professions LONGTEXT,times_claimed LONGTEXT,quests LONGTEXT,"
+			+ "waypoints LONGTEXT,friends LONGTEXT,skills LONGTEXT,bound_skills LONGTEXT,"
+			+ "class_info LONGTEXT,PRIMARY KEY (uuid));");
 	}
 
 	@Override
