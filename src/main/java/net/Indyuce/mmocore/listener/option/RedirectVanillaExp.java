5d6dcf6304608c0d84482e62c2d1b37ad102a928
Indyuce <cym.peyrat@yahoo.fr>
2022-07-09 20:03:36 +0200

    Small changes

--- src/main/java/net/Indyuce/mmocore/listener/option/RedirectVanillaExp.java
+++ src/main/java/net/Indyuce/mmocore/listener/option/RedirectVanillaExp.java
@@ -1,23 +1,20 @@
 package net.Indyuce.mmocore.listener.option;
 
+import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.experience.EXPSource;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
-import net.Indyuce.mmocore.api.player.PlayerData;
-
 public class RedirectVanillaExp implements Listener {
-	private final double ratio;
+    private final double ratio;
 
-	public RedirectVanillaExp(double ratio) {
-		this.ratio = ratio;
-	}
+    public RedirectVanillaExp(double ratio) {
+        this.ratio = ratio;
+    }
 
-	@EventHandler
-	public void a(PlayerExpChangeEvent event) {
-		double a = (event.getAmount() * ratio);
-		if (a > 0)
-			PlayerData.get(event.getPlayer()).giveExperience(a, EXPSource.VANILLA);
-	}
+    @EventHandler
+    public void a(PlayerExpChangeEvent event) {
+        PlayerData.get(event.getPlayer()).giveExperience(event.getAmount() * ratio, EXPSource.VANILLA);
+    }
 }
