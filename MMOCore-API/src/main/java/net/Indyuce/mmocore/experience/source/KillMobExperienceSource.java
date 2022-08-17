95ea032dfa51239053ce3cabbcb6b3320199951f
Indyuce <cym.peyrat@yahoo.fr>
2022-08-17 13:24:52 +0200

    New disp name option for killmob exp source

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/experience/source/KillMobExperienceSource.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/experience/source/KillMobExperienceSource.java
@@ -1,12 +1,13 @@
 package net.Indyuce.mmocore.experience.source;
 
+import io.lumine.mythic.lib.MythicLib;
 import io.lumine.mythic.lib.api.MMOLineConfig;
 import io.lumine.mythic.lib.api.event.PlayerKillEntityEvent;
 import net.Indyuce.mmocore.MMOCore;
 import net.Indyuce.mmocore.api.player.PlayerData;
-import net.Indyuce.mmocore.experience.source.type.SpecificExperienceSource;
 import net.Indyuce.mmocore.api.util.MMOCoreUtils;
 import net.Indyuce.mmocore.experience.dispenser.ExperienceDispenser;
+import net.Indyuce.mmocore.experience.source.type.SpecificExperienceSource;
 import net.Indyuce.mmocore.manager.profession.ExperienceSourceManager;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
@@ -16,23 +17,30 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.persistence.PersistentDataType;
 
+import javax.annotation.Nullable;
+
 public class KillMobExperienceSource extends SpecificExperienceSource<Entity> {
-    public final EntityType type;
+    private final EntityType type;
+
+    @Nullable
+    private final String displayName;
 
     public KillMobExperienceSource(ExperienceDispenser dispenser, MMOLineConfig config) {
         super(dispenser, config);
 
-        config.validate("type");
+        config.validateKeys("type");
+        displayName = config.contains("name") ? MythicLib.plugin.parseColors(config.getString("name")) : null;
         type = EntityType.valueOf(config.getString("type").toUpperCase().replace("-", "_").replace(" ", "_"));
     }
 
     @Override
     public ExperienceSourceManager<KillMobExperienceSource> newManager() {
         return new ExperienceSourceManager<KillMobExperienceSource>() {
+
             @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
             public void a(PlayerKillEntityEvent event) {
                 Bukkit.getScheduler().runTaskLater(MMOCore.plugin, () -> {
-                    if (event.getTarget().isDead() && !event.getTarget().getPersistentDataContainer().has(new NamespacedKey(MMOCore.plugin,"spawner_spawned"),PersistentDataType.STRING)) {
+                    if (event.getTarget().isDead() && !event.getTarget().getPersistentDataContainer().has(new NamespacedKey(MMOCore.plugin, "spawner_spawned"), PersistentDataType.STRING)) {
                         PlayerData data = PlayerData.get(event.getPlayer());
 
                         for (KillMobExperienceSource source : getSources())
@@ -46,6 +54,6 @@ public class KillMobExperienceSource extends SpecificExperienceSource<Entity> {
 
     @Override
     public boolean matchesParameter(PlayerData player, Entity obj) {
-        return obj.getType() == type;
+        return obj.getType() == type && (displayName == null || displayName.equals(obj.getCustomName()));
     }
 }
