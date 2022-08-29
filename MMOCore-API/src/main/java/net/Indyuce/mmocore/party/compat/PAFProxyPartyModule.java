ecde331aba52218af3aed6b4f4079dd9f472e18c
Simonsator <00pflaume@gmail.com>
2022-08-29 13:41:14 +0200

    Renamed PAF proxy party module

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/party/compat/PAFProxyPartyModule.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/party/compat/PAFProxyPartyModule.java
@@ -13,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PAFProxyExtendedPartyModule implements PartyModule {
+public class PAFProxyPartyModule implements PartyModule {
 
 	@Nullable
 	@Override
