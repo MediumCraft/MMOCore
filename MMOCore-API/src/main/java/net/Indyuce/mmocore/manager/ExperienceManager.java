d8cdd8791bd81a3b6651fec3359afb09bdab90a0
Indyuce <cym.peyrat@yahoo.fr>
2022-12-20 11:57:32 +0100

    Repair item exp source cleanup

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/manager/ExperienceManager.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/manager/ExperienceManager.java
@@ -1,16 +1,17 @@
 package net.Indyuce.mmocore.manager;
 
 import net.Indyuce.mmocore.MMOCore;
+import net.Indyuce.mmocore.api.ConfigFile;
 import net.Indyuce.mmocore.experience.ExpCurve;
 import net.Indyuce.mmocore.experience.droptable.ExperienceTable;
 import net.Indyuce.mmocore.experience.source.type.ExperienceSource;
 import net.Indyuce.mmocore.manager.profession.ExperienceSourceManager;
-import net.Indyuce.mmocore.api.ConfigFile;
 import org.apache.commons.lang.Validate;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.HandlerList;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
@@ -29,16 +30,15 @@ public class ExperienceManager implements MMOCoreManager {
     private final Map<Class<?>, ExperienceSourceManager<?>> managers = new HashMap<>();
 
     @SuppressWarnings("unchecked")
+    @Nullable
     public <T extends ExperienceSource> ExperienceSourceManager<T> getManager(Class<T> t) {
         return (ExperienceSourceManager<T>) managers.get(t);
     }
 
     @SuppressWarnings("unchecked")
     public <T extends ExperienceSource> void registerSource(T source) {
-        Class<T> path = (Class<T>) source.getClass();
-
-        if (!managers.containsKey(path))
-            managers.put(path, source.newManager());
+        final Class<T> path = (Class<T>) source.getClass();
+        managers.computeIfAbsent(path, unused -> source.newManager());
         getManager(path).registerSource(source);
     }
 
