b1b1a6eddd4dc2b3231025900223425a0aca4c7a
Ka0rX <guillaume.martinf@gmail.com>
2023-03-21 20:39:07 +0100

    UnlockSkillTrigger

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/api/quest/trigger/StatTrigger.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/api/quest/trigger/StatTrigger.java
@@ -4,11 +4,12 @@ import io.lumine.mythic.lib.api.MMOLineConfig;
 import io.lumine.mythic.lib.api.stat.modifier.StatModifier;
 import io.lumine.mythic.lib.player.modifier.ModifierType;
 import net.Indyuce.mmocore.api.player.PlayerData;
+import net.Indyuce.mmocore.api.quest.trigger.api.Removable;
 import org.apache.commons.lang.Validate;
 
 import java.util.UUID;
 
-public class StatTrigger extends Trigger {
+public class StatTrigger extends Trigger implements Removable {
     public static String TRIGGER_PREFIX = "mmocore_trigger";
     private final StatModifier statModifier;
     private final String stat;
@@ -42,6 +43,7 @@ public class StatTrigger extends Trigger {
      * opposite amount. (Little corrective term for the relative to have the inverse.
      * Not a problem to store twice the stat modifiers are there only remain in the RAM.
      */
+    @Override
     public void remove(PlayerData playerData) {
         playerData.getMMOPlayerData().getStatMap().getInstance(stat).remove(modifierKey);
     }
